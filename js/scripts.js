e9dc5021a6f34a95399e8ef5b3d9abe2b299cccf
Userman1986 <e.samonin1986@gmail.com>
2023-05-10 10:05:39 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
-let pokemonList = ['pokemon1', 'pokemon2', 'pokemon3'];
-
+let pokemonList = [pokemon1, pokemon2, pokemon3];
 let pokemon1 = {
-    name:'Bulbazaur',
-    height : [7],
-    type: 'grass'
+    name:"Bulbazaur",
+    height : 7,
+    type: "grass"
 }
 
 let pokemon2 = {
-    name:'Picachu',
-    height : [2],
-    type: 'lightning'
+    name:"Picachu",
+    height : 2,
+    type: "lightning"
 }
 
 let pokemon3 = {
-    name:'Charizard',
-    height : [10],
-    type: 'Fire'
+    name:"Charizard",
+    height : 10,
+    type: "Fire"
 }
