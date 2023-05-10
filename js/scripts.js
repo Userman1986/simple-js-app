67a7215703e2152c4de4582c1e2ff5e6cfdb2981
Userman1986 <e.samonin1986@gmail.com>
2023-05-10 08:35:06 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('Hello world');
-let favoriteFood = 'pelmeni';
-document.write(favoriteFood);
+let pokemonList = ['pokemon1', 'pokemon2', 'pokemon3'];
+
+let pokemon1 = {
+    name:'Bulbazaur',
+    height : [7],
+    type: 'grass'
+}
+
+let pokemon2 = {
+    name:'Picachu',
+    height : [2],
+    type: 'lightning'
+}
+
+let pokemon3 = {
+    name:'Charizard',
+    height : [10],
+    type: 'Fire'
+}
