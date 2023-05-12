e678ed89437b64a97dc7852921d0da18ee5faa9f
Userman1986 <e.samonin1986@gmail.com>
2023-05-12 09:46:04 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,21 @@ let pokemonList = [
         type: "Fire",
     },
 ];
+for (let i = 0; i <= pokemonList.length; i++) 
+{
+    if (pokemonList[i].height > 9)
+    {
+
+       
+        document.write("<p>" + pokemonList[i].name + ", Height=" + pokemonList[i].height, "  Wow its Big" + "</p>" );
+}
+else (pokemonList[i].height < 9)
 
 {
-for (let i = 0; i <= 0; i++) { document.write("<p>" + pokemonList[0].name, pokemonList[0].height + "</p>");}
-for (let i = 1; i <= 1; i++) { document.write("<p>" + pokemonList[1].name , pokemonList[1].height + "</p>");}
-for (let i = 2; i <= 2; i++) { document.write("<p>" + pokemonList[2].name, pokemonList[2].height + "</p>");}
-for (let i = 2; i <= 2; i++) { document.write("<p>" + pokemonList[2].name, "Height= 10", "Wow its Big" + "</p>");}
- }
+    document.write("<p>" + pokemonList[i].name + ", Height=" + pokemonList[i].height + "</p>" );
+}
+
+
+
+}
+    
